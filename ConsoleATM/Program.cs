fbfb695058a299928396943c41bd79efc915d83c
alstof <alstof@gmail.com>
2018-10-10 11:02:32 -0700

    Refactor account and add negative amount test

--- ConsoleATM/Program.cs
+++ ConsoleATM/Program.cs
@@ -2,28 +2,49 @@
 
 namespace ConsoleATM
 {
-    public class Program
+    public class Account
     {
-        protected static decimal _accountBalance = default(int);
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-        public static decimal GetBalance()
+        protected decimal _accountBalance = default(decimal);
+        public decimal GetBalance()
         {
             return _accountBalance;
         }
-        public static void DebitAccount(decimal value)
+        public void DebitAccount(decimal value)
         {
             decimal result = _accountBalance - value;
             if (result < 0) throw new Exception("Can't debit account below zero");
             else _accountBalance = result;
         }
-        public static void CreditAccount(decimal value)
+        public void CreditAccount(decimal value)
         {
             decimal result = _accountBalance + value;
             _accountBalance = result;
+        }
+    }
+    public class Program
+    {
+        public static void WithdrawFrom(Account acct, decimal amount)
+        {
+            if (amount < 0) throw new Exception("The sum couldn't be negative");
+            try
+            {
+                acct.DebitAccount(amount);
+            }
+            catch (Exception ex)
+            {
+                reportException(ex); 
+            }
+        }
+        public static void reportException(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        static void Main(string[] args)
+        {
+            Account acct = new Account();
+            acct.CreditAccount(100);
+            WithdrawFrom(acct, -6);
+            Console.ReadKey();
         }
     }
 }
--- ConsoleATMTests/UnitTest1.cs
+++ ConsoleATMTests/UnitTest1.cs
@@ -7,28 +7,45 @@ namespace ConsoleATMTests
     public class UnitTest1 : Program
     {
         [Fact]
-        public void ReturnBalance()
+        public void ReturnDefaultBalance()
         {
-            _accountBalance = 5000;
-            Assert.Equal(5000, GetBalance());
+            Account acct = new Account();
+            Assert.Equal(0, acct.GetBalance());
+        }
+
+
+        [Fact]
+        public void IncreaseBalanceBy100()
+        {
+            Account acct = new Account();
+            acct.CreditAccount(100);
+            Assert.Equal(0 + 100, acct.GetBalance());
         }
 
         [Fact]
-        public void DecreaseBalance()
+        public void DecreaseBalanceBy100()
         {
-            decimal accountBalanceValue = 5000;
-            _accountBalance = accountBalanceValue;
-            DebitAccount(100);
-            Assert.Equal(accountBalanceValue - 100, GetBalance());
+            Account acct = new Account();
+            acct.CreditAccount(1000);
+            acct.DebitAccount(100);
+            Assert.Equal(1000 - 100, acct.GetBalance());
         }
 
         [Fact]
-        public void IncreaseBalance()
+        public void CanWithdraw5()
         {
-            decimal accountBalanceValue = 5000;
-            _accountBalance = accountBalanceValue;
-            CreditAccount(100);
-            Assert.Equal(accountBalanceValue + 100, GetBalance());
+            Account acct = new Account();
+            acct.CreditAccount(1000);
+            WithdrawFrom(acct, 5);
+            Assert.Equal(1000 - 5, acct.GetBalance());
+        }
+        [Fact]
+        public void CantWithdrawNegative5()
+        {
+            Account acct = new Account();
+            acct.CreditAccount(1000);
+            Exception ex = Assert.Throws<Exception>(() => WithdrawFrom(acct, -5));
+            Assert.Equal("The sum couldn't be negative", ex.Message);
         }
     }
 }
